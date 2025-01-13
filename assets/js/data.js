df298a9924dff312a6f749750850da8b50fb5820
KhamdamovAva <73744057+Number1ava@users.noreply.github.com>
2025-01-13 17:42:37 +0500

    end AudioPlayer app

---
+++ assets/js/data.js
@@ -0,0 +1,43 @@
+
+export const data = [
+  {
+    id: 1,
+    link: "beat1.mp3",
+    genre: "Pop",
+    track: "Asap",
+    group: "Milow",
+    year: 2022,
+  },
+  {
+    id: 2,
+    link: "beat2.mp3",
+    genre: "Pop",
+    track: "Tek It",
+    group: "Cafune",
+    year: 2021,
+  },
+  {
+    id: 3,
+    link: "beat3.mp3",
+    genre: "Pop",
+    track: "Rumors",
+    group: "Jake Miller",
+    year: 2016,
+  },
+  {
+    id: 4,
+    link: "beat4.mp3",
+    genre: "Pop",
+    track: "Do you think About Me",
+    group: "Benjamin Ingrosso",
+    year: 2017,
+  },
+  {
+    id: 5,
+    link: "beat5.mp3",
+    genre: "Pop",
+    track: "Finaliste",
+    group: "Florina",
+    year: 2022,
+  },
+];
